20a081b3ccc6d76f1b9607367bb3840dd9b2422e
Irvin Medina <irvin.medina15@gmail.com>
2019-08-22 13:01:48 -0500

    hola

---
+++ 03.AreaTriangulo/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _03.AreaTriangulo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float area, b, a;
+            Console.WriteLine("Programam que calcula el area de un triangulo");
+
+        if (args.Length<2){
+            Console.WriteLine("Forma de uso");
+            Console.WriteLine("<base> <altura>");
+            Environment.Exit(0);
+           }
+
+        b=float.Parse(args[0]);
+        a=float.Parse(args[1]);
+
+        area= (b*a)/2;
+
+        Console.WriteLine($"El area es {area}");
+
+        }
+    }
+}
