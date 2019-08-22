e981cdf6ee87b06356146cf207c7f845a2ae995f
Irvin Medina <irvin.medina15@gmail.com>
2019-08-22 12:35:01 -0500

    hay va

---
+++ 02.AreaCirculo/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _02.AreaCirculo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double area;
+            double radio=0;
+            Console.Clear();
+            Console.WriteLine("Programa que calcula el area de un circulo");
+            Console.WriteLine("Dame el radio");
+            radio= float.Parse(Console.ReadLine());
+
+            area = Math.PI * Math.Pow(radio,2);
+            Console.Write($" El area es (area)");
+        }
+    }
+}
