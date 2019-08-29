0199c0c2829d01a3a8125779720c6687ea6b28ff
Irvin Medina <irvin.medina15@gmail.com>
2019-08-29 12:49:46 -0500

    subiendo

--- 09.VectorRandom/Program.cs
+++ 09.VectorRandom/Program.cs
@@ -6,7 +6,27 @@ namespace _09.VectorRandom
     {
         static void Main(string[] args)
         {
+            int[] A = new int[15];
+            int[] B = new int[15];
+            int[] C = new int[15];
+
+            Random rnd = new Random();
+
+            for(int i=0; i<A.Length; i++){
+                A[i]=rnd.Next(1,30);
+                B[i]=rnd.Next(1,30);
+                C[i]=rnd.Next(1,30);
+
+            }
+
+
+
             Console.WriteLine("Hello World!");
+        }
+        static void imprime(strig[] args){
+            for(int i== ;i<V.Length; i++)
+                Console.WriteLine("");
+
         }
     }
 }
