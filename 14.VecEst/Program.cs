cf6ec995013c7a36b4c60e177d84df328d8198f9
Irvin Medina <irvin.medina15@gmail.com>
2019-08-29 13:18:44 -0500

    subiendo

---
+++ 14.VecEst/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace _14.VecEst
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] A;
+            int n=0;
+            Console.WriteLine("Cuantos elementos? "); n = int.Parse(Console.ReadLine());
+            A= new double[n];
+
+            Console.WriteLine("Dame el elemento del arreglo \n");
+            leer(A);
+
+            Console.WriteLine("Estadisticas");
+            Console.WriteLine($"Mayor {mayor[A]}");
+            Console.WriteLine($"Menor {menor[A]}");
+            Console.WriteLine($"Promedio {promedio[A]}");
+            Console.WriteLine($"Varianza {varianza(A,promedio[A])}");
+            Console.WriteLine($"Desv Estandar {Math.Sqrt(varianza(A,promedio[A]))}");
+        }
+
+        static double varianza(double[] v, double media){
+            double s=0;
+            for(int i = 1; i<v.Length ; i++)
+                s+= Math.Pow(v.[i]-promedio,2);
+            return s/v.Length-1;
+
+        }
+
+        static double promedio (double[] v){
+            double s=0;
+            for(int i = 0; i<v.Length ; i++)
+                s+= v.[i];
+            return s/v.Length;
+            }
+
+        static double menor (double[] v){
+            double m=v[0];
+                if (v[i]< m) m=v[i];
+            for(int i = 1; i<v.Length ; i++)
+            return m;
+            }
+
+        static double mayor (double[] v){
+            double m=v[0];
+            for(int i = 1; i<v.Length ; i++)
+                if (v[i]> m) m=v[i];
+            return m;
+            }
+
+        static void leer(double[], v){
+            for(int i=0;i<v.Length;i++){
+                Console.Write($"Elemento {i+} =" ); 
+                v[i]=double.Parse(Console.ReadLine());
+            }
+        }
+    }
+}
