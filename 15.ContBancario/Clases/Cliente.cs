0e134af48d02b91e5f07cd1fdad728119f31b292
Irvin Medina <irvin.medina15@gmail.com>
2019-09-05 13:11:24 -0500

    subiendo

---
+++ 15.ContBancario/Clases/Cliente.cs
@@ -0,0 +1,23 @@
+using System.Collections.Generic;
+namespace ContBancario.Clases{
+    public class Cliente{
+        private string nombre;
+        private List<CuentaBancaria> cuentas;
+
+        public Cliente (string nombre){
+            this.nombre=nombre;
+            cuentas = new List<CuentaBancaria>();
+        }
+
+        public string Nombre{
+            get {return nombre;}
+        }
+        public List<CuentaBancaria> Cuentas{
+            get {return cuentas;}
+        }
+
+        public void AgregarCuenta(CuentaBancaria cta){
+            cuentas.Add(cta);
+        }
+    }
+}
--- 15.ContBancario/Program.cs
+++ 15.ContBancario/Program.cs
@@ -25,6 +25,47 @@ namespace ContBancario
             Console.WriteLine($"Cuenta 3 : {miahorro.Saldo}");
             Console.WriteLine($"Cuenta 4 : {micheque.Saldo}");
 
+            //probar cliente y banco
+
+            Banco MiBanco= new Banco ("Banco del Billeton");
+
+            Cliente cte1 = new Cliente("Chente fox");
+            Cliente cte2 = new Cliente("Feli pisto ");
+            Cliente cte3 = new Cliente("quique peña");
+            Cliente cte4 = new Cliente("amlover");
+
+            cte1.AgregarCuenta( new CuentaCheques(2000,1000));
+            cte2.AgregarCuenta( new CuentaCheques(1000,200));
+            cte3.AgregarCuenta( new CuentaCheques(900,100));
+            cte4.AgregarCuenta( new CuentaCheques(12000,1000));
+            cte4.AgregarCuenta( new CuentaAhorro(2000,0.3));
+
+            cte1.Cuentas[0].Retirar(300);
+            cte3.Cuentas[0].Deposita(1200);
+            cte4.Cuentas[1].Retirar(1000);
+
+            MiBanco.AgregarCliente(cte1);
+            MiBanco.AgregarCliente(cte2);
+            MiBanco.AgregarCliente(cte3);
+            MiBanco.AgregarCliente(cte4);
+
+            MiBanco.AgregarCliente(new Cliente("Tello "));
+            MiBanco.Clientes[4].AgregarCuenta(new CuentaCheques(3000,900));
+
+            Console.WriteLine("------------Reporte de Clientes-------------");
+            Console.WriteLine($"{MiBanco.Nombre}");
+
+            foreach (Cliente cte in MiBanco.Clientes){
+                Console.WriteLine($"Cliente: {cte.Nombre}");
+                foreach (CuentaBancaria cta in cte.Cuentas){
+                    if(cta is CuentaAhorro)
+                        Console.WriteLine($"Cuenta de Ahorros {cta.Saldo}");
+                    else
+                        Console.WriteLine($"Cuenta de Cheques {cta.Saldo}");
+                }
+                Console.WriteLine(" ");
+            }
+
         }
     }
 }
