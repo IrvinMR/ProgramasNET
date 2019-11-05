17279319a942b0e0e44bb5e341886f50e197587e
Irvin Medina <irvin.medina15@gmail.com>
2019-11-05 13:24:06 -0600

    subiendo

---
+++ dvdCollection_v4/Data/dvdCollectionContex.cs
@@ -0,0 +1,31 @@
+//el contexto es un archivo especial para las llaves primarias de las bases de datos
+using Microsoft.EntityFrameworkCore;
+using dvdCollection.Models;
+
+
+namespace dvdCollection.Data 
+{
+    public class dvdCollectionContext : DbContext
+    {
+        public dvdCollectionContext(DbContextOptions<dvdCollectionContext> options) : base(options) 
+        {
+        }
+
+        public DbSet<Actors> Actors {get; set;}
+        public DbSet<FilmCertificates> FilmCertificates {get; set;}
+        public DbSet<FilmGeneres> FilmGeneres {get; set;}
+        public DbSet<FilmsActorRoles> FilmsActorRoles {get; set;}
+        public DbSet<FilmTitles> FilmTitles {get; set;}
+        public DbSet<FilmTitlesProducers> FilmTitlesProducers {get; set;}
+        public DbSet<Producers> Producers {get; set;}
+        public DbSet<RoleTypes> RoleTypes {get; set;}
+
+
+        //sobrecargar metodo para llaves primarias compartidas
+        protected override void OnModelCreating(ModelBuilder modelBuilder){
+            modelBuilder.Entity<FilmTitlesProducers>().HasKey(c => new {c.ProducerID, c.FilmTitleID});
+            modelBuilder.Entity<FilmsActorRoles>().HasKey(c => new {c.FilmTitleID, c.ActorID, c.RoleTypeID});
+        }
+
+    }
+}
---
+++ dvdCollection_v4/Models/Producers.cs
@@ -0,0 +1,22 @@
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace dvdCollection.Models {
+
+    public class Producers {
+        [Key]
+        [Display(Name="ID")]
+        public int ProducerID {get; set;}
+        [Display(Name="Name",Prompt="Nombre completo del Productor")]
+        [Required(ErrorMessage="Nombre del productor obligatorio")] //para que el campo sea obligatorio
+        [StringLength(80)]
+        public string ProducerName {get; set;}
+        [Display(Name="Email",Prompt="Correo Electronico")]
+        [DataType(DataType.EmailAddress)]
+        public string ContactEmailAddress {get; set;}
+        [DataType(DataType.Url)]
+        public string WebSite {get; set;}
+
+        public ICollection<FilmTitlesProducers> FilmTitlesProducers {get; set;}
+    }
+}
